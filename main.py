53bd2ad12ac0719484d42729f398d649f7ad4ef1
abdikaalbiyan <abdikaalbiyan@gmail.com>
2021-04-30 00:02:02 +0700

    update

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import numpy as np
 
 def main():
 
-    data1 = pd.read_json('gs://week3-spark-etl/json-files/2019-05-04.json', lines=True)
+    data1 = pd.read_json('gs://week3-spark-etl/json-files/2021-04-27.json', lines=True)
     data1.drop('id', inplace=True, axis=1)
     data1.loc[data1['flight_date'] == '2019-05-04', 'flight_date'] = '2021-04-26'
 
-    data2 = pd.read_json('gs://week3-spark-etl/json-files/2019-05-05.json', lines=True)
+    data2 = pd.read_json('gs://week3-spark-etl/json-files/2021-04-28.json', lines=True)
     data2.drop('id', inplace=True, axis=1)
     data2.loc[data2['flight_date'] == '2019-05-05', 'flight_date'] = '2021-04-27'
 
-    data3 = pd.read_json('gs://week3-spark-etl/json-files/2019-05-06.json', lines=True)
+    data3 = pd.read_json('gs://week3-spark-etl/json-files/2021-04-29.json', lines=True)
     data3.drop('id', inplace=True, axis=1)
     data3.loc[data3['flight_date'] == '2019-05-06', 'flight_date'] = '2021-04-28'
 
-    data4 = pd.read_json('gs://week3-spark-etl/json-files/2019-05-07.json', lines=True)
+    data4 = pd.read_json('gs://week3-spark-etl/json-files/2021-04-30.json', lines=True)
     data4.drop('id', inplace=True, axis=1)
     data4.loc[data4['flight_date'] == '2019-05-07', 'flight_date'] = '2021-04-29'
 
